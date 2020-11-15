774e5dbb54fdccb2688d8b042b57d0e43c72d932
mamaluigie <mamaluigie@me.com>
2020-11-15 14:12:18 -0600

    Finished with the main menu skeleton

--- src/gui.py
+++ src/gui.py
@@ -4,36 +4,25 @@
 # --------------------------------------------------------------------------------------------------
 # Imports
 import tkinter as tk
+import sys
 from tkinter import ttk
+from tkinter import messagebox
+import pdb
 
 # ==================================================================================================
-class GUI():
-
-    # Global variable for file location of software to scan
-    file_location = ""
+class GUI(tk.Tk):
 
 # ==================================================================================================
-    # Button Functions for Windows
+    # This will be used to display the main menu when called
+    def __init__(self):
+        super().__init__()
 
-    # Function used to open other window that will be used for the script based attacks
-    def script_attack_window(self):
-        pass
+        self.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.title("Main Menu")
+        self.attributes('-type', 'dialog')
 
-    # Close Window Button Command
-    def close_window(self, window):
-        return window.quit
+        self.start_user_interface()
 
-    # Start Audit Button Command
-    def start_audit(self):
-        #function to start the security audit
-        pass
-
-    # Display Text To Textbox Command
-    def display_text_to_textbox(self, text):
-        self.display_textbox.insert(tk.END, text)
-        pass
-
-# ==================================================================================================
     # Main Window Function
 
     # Rescource for using frames to organize the window better
@@ -41,73 +30,81 @@ class GUI():
     # https://www.tutorialspoint.com/python/tk_frame.htm
     # https://realpython.com/python-gui-tkinter/
     # https://tkdocs.com/tutorial/
+
+    # Functions for the buttons
+    def on_close(self):
+        if messagebox.askokcancel(self.title(), "Press ok to exit", parent=self):
+            self.destroy()
     
-    # This will be used to display the main menu when called
-    def __init__(self):
-
-        # Initializing a new window
-        root = tk.Tk()
-        root.title("Main Menu")
-    
-        # Making frames
-        canvas = tk.Frame()
-        blocked_space = tk.Frame(canvas)
-
-        # Initializing the window settings
-        root.attributes('-type', 'dialog')
-#=======================Buttons=========================
-
-        # Button to start the autiting software
-        auditing_button = tk.Button(master=canvas, text="Audit", command=self.start_audit())
-
-        # Button to close the window
-        close_button = tk.Button(master=canvas, text="Close", command=self.close_window(root))
-
-        # Button to open script page
-        script_button = tk.Button(master=canvas, text="Script Attack", command=self.script_attack_window())
-
-        # Textbox for getting user input for file location
-        self.file_location_entry= tk.Entry(master=canvas)
-
-        # Button to set the file variable
-        file_enter_button = tk.Button(master=canvas, text="Enter File Path", command=self.set_variable())
-
-        # TextBox for displaying the text
-        self.display_textbox = tk.Text(master=canvas) 
-        
-        # Test Display of Text
-        self.display_text_to_textbox("Test text")
-
-        # Plotting the buttons on the grid
-        canvas.grid(column=0, row=0)
-
-        auditing_button.grid(row=3, column=1)
-        auditing_button.columnconfigure(1, weight=1)
-
-        close_button.grid(row=3, column=0)
-        close_button.columnconfigure(0, weight=1)
-
-        script_button.grid(row=3, column=2)
-        script_button.columnconfigure(2, weight=1)
-
-        self.file_location_entry.grid(column=0, row=1)
-        file_enter_button.grid(column=0, row=0)
-        self.display_textbox.grid(column=3, row=1)
-
-
-        # Starting the window mainloop
-        root.mainloop()
-    
-# ==================================================================================================
-    # Other Window Functions
-
-    def set_variable(self):
-        self.file_location = self.file_location_entry.get()
-
-    # Window for entering default location for auditing tool
-    def default_location_auditing_tool_window(self):
+    def start_audit(self):
         pass
+        
+    def start_script_attack(self):
+        pass
+
+    def std_out(self, text):
+        self.textbox.insert(tk.END, str(text + '\n'))
+
+    def set_file_location(self):
+        self.file_location = self.file_entry.get()
+        self.std_out(self.file_location)
+
+    def clear_stdout(self):
+        self.textbox.delete('1.0', tk.END)
+
+    def start_user_interface(self):
+        
+        # File location variable
+        self.file_location = tk.StringVar()
+
+        # Column and Row Variables to use
+        column = 0
+        row = 0
+
+        # Making the labelframes
+        self.button_frame = ttk.Labelframe(self, text="Buttons", padding=5)
+        self.file_location_frame = ttk.Labelframe(self, text="File Location")
+        self.std_out_frame = ttk.Labelframe(self, text="STD Out Window")
+
+        # Initializing the buttons for the buttons labelframe
+        buttons = [
+                ["Audit", self.start_audit],
+                ["Script", self.start_script_attack],
+                ["Clear", self.clear_stdout],
+                ["Close", self.on_close]
+                ]
+
+        # Packing the button frame
+        for button in buttons:
+            ttk.Button(self.button_frame, command=button[1], text=button[0]).grid(column = 0, row = row, sticky='n', pady = 5)
+            row += 1
+        
+        # Initializing and packing the file location labelframe stuff
+        ttk.Button(self.file_location_frame, text="Enter Filelocation", command=self.set_file_location).grid(column=0, row=0, sticky="nw")
+
+        # Initializing and packing the file entry entrybox
+        self.file_entry = ttk.Entry(self.file_location_frame, textvariable=self.file_location, width=80)
+        self.file_entry.grid(column=0, row=1, sticky="we", pady=0)
+
+        # Initializing and packing the stdout labelframe and textbox
+        self.textbox = tk.Text(self.std_out_frame)
+        self.textbox.grid(row=0, column=0)
+
+        # Initializing and packing the scrollbar for teh textbox
+        self.scrollbar = tk.Scrollbar(self.std_out_frame)
+        self.scrollbar.grid(column=1, row=0)
+
+        self.textbox.config(yscrollcommand=self.scrollbar)
+        self.scrollbar.config(command=self.textbox.yview)
+
+        # Packing all of the frames
+        self.file_location_frame.grid(column = 1, row = 0, sticky="new")
+        self.std_out_frame.grid(column = 1, row = 1, sticky="n")
+        self.button_frame.grid(column = 0, row=0, sticky="n", rowspan=2)
+
+# ==================================================================================================
 
 # ---------------------------------------------------------------------------------------------------
 
 x = GUI()
+x.mainloop()
